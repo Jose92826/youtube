dc4e60b854355d8193d41ed4bee3996733c3a802
ayd.1 <jose92826@hotmail.com>
2013-12-10 22:00:23 -0600

    Creacion inicial de repositorio.

---
+++ youtube/src/youtube/base.java
@@ -0,0 +1,14 @@
+package youtube;
+
+public class base {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		suma(1,2);
+	}
+	
+	public static void suma(int x, int y){
+		System.out.println(x+y);
+	}
+
+}
